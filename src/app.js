11cf5e4b9b708c0ba74b5d9ddd157081827e486a
davidmgudeman <davidmgudeman@gmail.com>
2017-09-05 14:19:07 -0700

    added firebase to project for email/password authentication

--- src/app.js
+++ src/app.js
@@ -2,9 +2,21 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
 import reducers from './reducers';
 
 class App extends Component {
+  componentWillMount() {
+    const config = {
+      apiKey: 'AIzaSyDnuMg-uVZFriRBjlrdIZQqB02GvzjYFvg',
+      authDomain: 'manager-6caa5.firebaseapp.com',
+      databaseURL: 'https://manager-6caa5.firebaseio.com',
+      projectId: 'manager-6caa5',
+      storageBucket: '',
+      messagingSenderId: '1072852811059'
+    };
+    firebase.initializeApp(config);
+  }
   render() {
     return (
       <Provider store={createStore(reducers)}>
