0b9f32cb1bff0b52224cbcb294e17a4ebef6dba6
davidmgudeman <davidmgudeman@gmail.com>
2017-09-06 08:33:40 -0700

    added login_user_fail

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Text } from 'react-native';
 import { connect } from 'react-redux';
 import { 
   emailChanged, 
@@ -47,6 +48,11 @@ class LoginForm extends Component {
             value={this.props.password}
           />
         </CardSection>
+        
+        <Text style={styles.errorTextStyle}>
+          {this.props.error}
+        </Text>
+
         <CardSection>
           <Button onPress={this.onButtonPress.bind(this)}>
             Login
@@ -57,11 +63,17 @@ class LoginForm extends Component {
   }
 }
 
-const mapStateToProps = state => {
-  return {
-    email: state.auth.email,
-    password: state.auth.password,
-  };
+const styles = {
+  errorTextStyle: {
+    fontSize: 20,
+    alignSelf: 'center',
+    color: 'red'
+  }
+};
+
+const mapStateToProps = ({ auth }) => {
+  const { email, password, error } = auth;
+  return { email, password, error, };
 };
 
 export default connect(mapStateToProps, { 
