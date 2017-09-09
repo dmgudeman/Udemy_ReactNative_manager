5fb35ed797f799bef7b01472bdc6aab02865b0b1
davidmgudeman <davidmgudeman@gmail.com>
2017-09-08 17:35:54 -0700

    finished the app

--- src/reducers/EmployeeFormReducer.js
+++ src/reducers/EmployeeFormReducer.js
@@ -1,7 +1,8 @@
 
 import {
   EMPLOYEE_UPDATE,
-  EMPLOYEE_CREATE
+  EMPLOYEE_CREATE,
+  EMPLOYEE_SAVE_SUCCESS
 } from '../actions/types';
 
 const INITIAL_STATE = { 
@@ -17,6 +18,8 @@ export default (state = INITIAL_STATE, action) => {
       // the [] is key interpolation
       return { ...state, [action.payload.prop]: action.payload.value };
     case EMPLOYEE_CREATE:
+      return INITIAL_STATE;
+    case EMPLOYEE_SAVE_SUCCESS:
       return INITIAL_STATE;
     default:
       return state;
